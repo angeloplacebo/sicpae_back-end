08bcbfb51289f7d52e057623970fa640c992a29b
Angelo Johnson <public.angelo@gmail.com>
2022-07-10 00:13:52 -0300

    feat: add record endpoints/routes

--- src/v1/routes/record.ts
+++ src/v1/routes/record.ts
@@ -4,6 +4,20 @@ import RecordController from '../controllers/recordController'
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  res.send('server is running!')
+})
+
+routes.get('/api/v1/index', RecordController.getAll);
+routes.get('/api/v1/current', RecordController.getCurrent);
+routes.get('/api/v1/today', RecordController.today);
+routes.get('/api/v1/day', RecordController.getDay);
+routes.get('/api/v1/day_details', RecordController.getDayDetails);
+routes.get('/api/v1/month', RecordController.getMonth);
+routes.get('/api/v1/month_details', RecordController.getMonthDetails);
+routes.get('/api/v1/year', RecordController.getYear);
+routes.get('/api/v1/year_details', RecordController.getYearDetails);
+
 routes.post('/api/v1/register', RecordController.create);
 
 export default routes;
