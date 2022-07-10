22be37a8e243de084cab6475250566b4ad19cd79
Angelo Johnson <public.angelo@gmail.com>
2022-07-09 23:18:10 -0300

    feat: add pagination options

--- src/v1/controllers/recordController.ts
+++ src/v1/controllers/recordController.ts
@@ -2,6 +2,13 @@ import { Response, Request } from 'express';
 
 import recordService from '../services/recordService';
 
+const PageOptions = (req: Request) => {
+  return {
+    page: parseInt(req.query.page as string, 10) -1 || 0,
+    limit: parseInt(req.query.limit as string, 10) || 10
+  }
+}
+
 const RecordController = {
 
 
